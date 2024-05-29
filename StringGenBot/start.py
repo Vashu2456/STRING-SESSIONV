e114a3ff791ceec5b14f0de6b813db030bb768fd
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-05-29 10:29:23 +0530

    Create start.py

---
+++ StringGenBot/start.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start" , "vashustart"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""Hᴇʏ {msg.from_user.mention}🦋,
+
+Tʜɪs ɪs {me2},
+Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+Jo nibha na saku yesa wada nahi krta🥂 
+ 
+Mai baat apne okaat se jayda nahi krta🍺 
+ 
+Bhale Tamanna rakhta hu asaan chune ki🍻 
+ 
+Magar kissi ko girane ka irada nahi rakhta 🍷.
+
+Mᴀᴅᴇ ᴡɪᴛʜ ❤ ʙʏ : [VASHU](https://t.me/Vashu23456) !""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("sᴏᴜʀᴄᴇ", url="https://t.me/ALL_BOTS_SUPPORT1"),
+                    InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴩᴇʀ", url="https://t.me/BRANDEDKING82")
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
