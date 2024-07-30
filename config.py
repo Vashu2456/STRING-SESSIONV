702c5a11c9c842f7c20c9dbad69dc03cff25cf81
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-07-30 20:01:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "25581940"))
+API_HASH = getenv("API_HASH" , "1e80acd755ceab02b3e2a22d49b25c18")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-#OWNER_ID = int(getenv("1321796330"))
-OWNER_ID = int(getenv("OWNER_ID", "1321796330"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+BOT_TOKEN = getenv("BOT_TOKEN" , "7344614680:AAHzbJqWxPLErNB63AgOcgxSYYLyBstJT3c")
+#OWNER_ID = int(getenv("7496416021"))
+OWNER_ID = int(getenv("OWNER_ID", "7496416021"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", " mongodb+srv://vobero4050:rMDlUdxhAw9MDeaO@cluster2345908.hlfgzxh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2345908")
+MUST_JOIN = getenv("MUST_JOIN", "@DEMONSQUAD001")
