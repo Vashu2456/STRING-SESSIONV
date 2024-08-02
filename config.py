a8c87284064b2590742ace5173547425bfa355a0
SaTyAm0084 <125863967+SaTyAm0084@users.noreply.github.com>
2024-08-02 09:44:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ BOT_TOKEN = getenv("BOT_TOKEN" , "7344614680:AAHzbJqWxPLErNB63AgOcgxSYYLyBstJT3c
 #OWNER_ID = int(getenv("7496416021"))
 OWNER_ID = int(getenv("OWNER_ID", "7496416021"))
 MONGO_DB_URI = getenv("MONGO_DB_URI", " mongodb+srv://vobero4050:rMDlUdxhAw9MDeaO@cluster2345908.hlfgzxh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2345908")
-MUST_JOIN = getenv("MUST_JOIN", "@DEMONSQUAD001")
+MUST_JOIN = getenv("MUST_JOIN", "DEMON_SQUAD_001")
