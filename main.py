e62004186043b3f52ca74784cb7da1521e83d54f
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-11-21 12:20:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = Client(
 if __name__ == "__main__":
     print("𝖲𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝖸𝐨𝐮𝐫 𝖲𝐭𝐫𝐢𝐧𝐠 𝖡𝐨𝐭...")
     try:
-        app.start()
+        app,start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
         raise Exception("Your API_ID/API_HASH is not valid.")
     except AccessTokenInvalid:
